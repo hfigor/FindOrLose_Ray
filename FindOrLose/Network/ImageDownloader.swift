52fdc00139a002fca48bdd843d41129cce2cf170
Frank Cipolla <frankc@cscit.net>
2021-11-08 07:48:24 -0500

    Finished imageDownloader. will not build yet

--- FindOrLose/Network/ImageDownloader.swift
+++ FindOrLose/Network/ImageDownloader.swift
@@ -28,22 +28,60 @@
 
 import Foundation
 import UIKit
+import Combine
 
 enum ImageDownloader {
-  static func download(url: String, completion: @escaping (UIImage?) -> Void) {
-    let url = URL(string: url)!
-
-    URLSession.shared.dataTask(with: url) { data, response, error in
-      guard
-        let httpURLResponse = response as? HTTPURLResponse,
-        httpURLResponse.statusCode == 200,
-        let data = data, error == nil,
-        let image = UIImage(data: data)
+  static func download(url: String) -> AnyPublisher<UIImage, GameError> {
+    guard let url = URL(string: url) else {
+      return Fail(error: GameError.invalidURL)
+        .eraseToAnyPublisher()
+    }
+    
+    
+    return URLSession.shared.dataTaskPublisher(for: url)
+      /// Returns a publisher that wraps a URL session data task for a given URL request.
+      ///
+      /// The publisher publishes data when the task completes, or terminates if the task fails with an error.
+      /// - Parameter request: The URL request for which to create a data task.
+      /// - Returns: A publisher that wraps a data task for the URL request.
+      .tryMap { response -> Data in
+        guard
+          let httpURLResponse = response.response as? HTTPURLResponse,
+          httpURLResponse.statusCode == 200
         else {
-          completion(nil)
-          return
+          throw GameError.statusCode
+        }
+        
+        return response.data
       }
-      completion(image)
-    }.resume()
+    
+      .tryMap {data in
+        guard let image = UIImage(data: data) else {
+          throw GameError.invalidImage
+        }
+        return image
+      }
+      .mapError { GameError.map($0) }
+      .eraseToAnyPublisher()
+      /// Use ``Publisher/eraseToAnyPublisher()`` to expose an instance of ``AnyPublisher`` to the downstream subscriber, rather than this publisher’s actual type.
+      /// This form of _type erasure_ preserves abstraction across API boundaries, such as different modules.
+      /// When you expose your publishers as the ``AnyPublisher`` type, you can change the underlying implementation over time without affecting existing clients.
+      ///
+  // Ditch the old closure way of doing things:
+//    let url = URL(string: url)!
+//
+//    URLSession.shared.dataTask(with: url) { data, response, error in
+//      guard
+//        let httpURLResponse = response as? HTTPURLResponse,
+//        httpURLResponse.statusCode == 200,
+//        let data = data, error == nil,
+//        let image = UIImage(data: data)
+//        else {
+//          completion(nil)
+//          return
+//      }
+//      completion(image)
+//    }.resume()
   }
 }
+
